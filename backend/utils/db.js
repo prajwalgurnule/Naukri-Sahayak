8b91dd11bcbc0950cdde2ae5ad5266e034ff261e
Prajwal Gurnule <117385840+prajwalgurnule@users.noreply.github.com>
2024-11-21 15:22:02 +0530

    Delete backend directory

--- backend/utils/db.js
+++
@@ -1,5 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from '../utils/schema.js'
-const sql = process.env.NEXT_PUBLIC_DRIZZLE_DB_URL;
-export const db = drizzle(sql,{schema});
